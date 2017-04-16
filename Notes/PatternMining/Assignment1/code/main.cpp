a0103978d6f4794df071da7b83fd547bc292dfdd
hechengwang <hecheng.wang@gmail.com>
2017-04-15 23:14:44 -0500

    added cpp code

---
+++ Notes/PatternMining/Assignment1/code/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  PatternMiningProgrammingAssignment1
+//
+//  Created by Hecheng Wang on 4/6/17.
+//  Copyright © 2017 Hecheng Wang. All rights reserved.
+//
+
+
+#include "getL1Dict.hpp"
+#include "getLMultDict.hpp"
+#include "getCombDict.hpp"
+using namespace std;
+
+int main() {
+    string input_path = "/Users/hechengwang/Main/Notes/PatternRecognition/Assignment1/data.txt";
+    string output_path = "/Users/hechengwang/Main/Notes/PatternRecognition/Assignment1/patterns.txt";
+    long minsup = 771;
+
+    vector<string> comboList = getL1(minsup, input_path, output_path);
+
+    long k = 2;
+    while (comboList.size() > 1)
+    {
+        vector<vector<string>> v = getCombs(k, comboList);
+        comboList = getLMult(k, minsup, v, input_path, output_path);
+        k++;
+    }
+    return 0;
+}
+
