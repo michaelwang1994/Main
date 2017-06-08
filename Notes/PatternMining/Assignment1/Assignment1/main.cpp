d9d7d9503844b9fd045f98025dc713d7a959d53b
hechengwang <hecheng.wang@gmail.com>
2017-06-08 00:29:19 -0500

    removed unnecessary files

---
+++ Notes/PatternMining/Assignment1/Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Hecheng Wang on 4/28/17.
+//  Copyright © 2017 Hecheng Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
